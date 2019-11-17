14b9c9e4d7a55e01012a7910cdb963587b8625c9
Nitai Dean <nitaidean@gmail.com>
2019-11-16 19:08:23 -0500

    1. adding function to manually add elements from a website by copy/pasting into console 2. adding working function to load all listener elements from backend and add tracking to them (dont know if it works with google analytics yet)

--- js/discern.js
+++ js/discern.js
@@ -1,32 +1,42 @@
 class Discern {
     constructor(user_api) {
-        var listen_targets = this.getAnalysis();
-        if (listen_targets == null) {
-            this.analyze();
-        }
-        this.applyListeners()
+        this.getAnalysis();
+        this.analyze()
     }
 
     getAnalysis() {
-        // this will query our server for whether webpage for instruction on where to listen
-        return null
+        // Queries the backend for all elements on this page
+        var xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4 && this.status === 200) {
+                applyListeners(JSON.parse(this.responseText));
+            }
+        };
+        const url = 'https://discern-app.herokuapp.com/get_all_elements';
+        // const url = 'http://localhost:5000/get_all_elements';
+        const data = JSON.stringify(
+            {'domain': document.location.host,
+                    'page': document.location.pathname});
+        xhr.open("POST", url, true);
+        xhr.send(data);
     }
-
 
     analyze() {
         // This function reports the current html page to our webserver
-        var bodyHtml = document.getElementsByTagName('body')[0].innerHTML;
         // we also want to expand every "relative path" resource. this is TBD.
-
         const Http = new XMLHttpRequest();
-        const url = 'https://webhook.site/b6415650-1dfc-4c0c-87c1-be90b33ac9e5';
-
+        const url = 'https://discern-app.herokuapp.com/analyze_page';
+        // const url = 'http://localhost:5000/analyze_page';
+        // var bodyHtml = document.getElementsByTagName('body')[0].innerHTML;
+        const data = JSON.stringify(
+            {'domain': document.location.host,
+                    'page': document.location.pathname,
+                    'html': ''});
         Http.open("POST", url, true);
-        Http.send(bodyHtml);
-
+        Http.send(data);
     }
 
-    report_event(event, payload = null) {
+    reportEvent(event, payload=null) {
         if (typeof gtag !== 'undefined') {
             gtag('event', event)
         }
@@ -35,26 +45,81 @@ class Discern {
             analytics.track(event, payload)
         }
     }
+}
 
 
-    applyListeners() {
-        var all_buttons = document.getElementsByTagName("button");
-
-        for (let button of all_buttons) {
-            let button_name = this.name_button(button);
-            console.log(button);
-            button.addEventListener('click', () => this.report_event(button_name + ' clicked'))
+function applyListeners(elementInstructions) {
+    const keys = Object.keys(elementInstructions);
+    for (let key of keys) {
+        let elementInstruction = elementInstructions[key];
+        var element = null;
+        let elementName = '';
+        if (elementInstruction['id'] !== '') {
+            element = document.getElementById(elementInstruction['id']);
+            elementName = nameElement(element)
         }
-    }
-
-    name_button(button) {
-        if (button.id) {
-            return button.id;
+        else if (elementInstruction['className'] !== '') {
+            let classElements = document.getElementsByClassName(elementInstruction['className']);
+            element = classElements[elementInstruction['classIndex']];
+            elementName = nameElement(element, elementInstruction['classIndex'])
         }
-        if (button.innerText) {
-            return button.innerText;
+        if (element !== null) {
+            console.log(element);
+            element.addEventListener('click', () => this.reportEvent(elementName + ' clicked'))
         }
-        return 'unkown_button'
     }
 }
 
+
+function nameElement(element, classIdx=null) {
+    if (element.id) {
+        return element.id;
+    }
+    if (element.innerText) {
+        return element.innerText;
+    }
+    if (element.className && classIdx) {
+        return element.className + "_" + classIdx;
+    }
+    return 'unknown_button'
+}
+
+
+function WriteElementToDB(document) {
+    var output_json = {
+        'domain': document.location.host,
+        'page': document.location.pathname,
+        'instructions': {
+            'id': '',
+            'className': '',
+            'classIndex': ''}
+    };
+    var addElement = false;
+
+    if (document.activeElement.id !== "") {
+        output_json['instructions']['id'] = document.activeElement.id;
+        addElement = true;
+    }
+    else if (document.activeElement.className !== "") {
+        const classElements = document.getElementsByClassName(document.activeElement.className);
+        var i;
+        var classIndex = -1;
+        for (i = 0; i < classElements.length; i++) {
+            if (classElements[i] === document.activeElement) {
+                classIndex = i;
+            }
+        }
+        output_json['instructions']['className'] = document.activeElement.className;
+        output_json['instructions']['classIndex'] = classIndex;
+        addElement = true;
+    }
+
+    if (addElement) {
+        const Http = new XMLHttpRequest();
+        const url = 'https://discern-app.herokuapp.com/add_element';
+        // const url = 'http://localhost:5000/add_element';
+        const data = JSON.stringify(output_json);
+        Http.open("POST", url, true);
+        Http.send(data);
+    }
+}
